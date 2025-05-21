d84e615a1911f87df65bc9b3323c402f39d39a0d
Henrique Ferreira Polizer <polizerrickk@gmail.com>
2025-05-21 13:27:13 -0300

    feat: criado metodo para conversão de agendamento para dto

--- src/main/java/com/barberbross/barberbross/dto/AgendamentDTO.java
+++ src/main/java/com/barberbross/barberbross/dto/AgendamentDTO.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 
 import com.barberbross.barberbross.enums.StatusAgendamento;
+import com.barberbross.barberbross.model.Agendamento;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import lombok.Getter;
@@ -26,4 +27,20 @@ public class AgendamentDTO {
     private Long servicoId;
     private Long barbeariaId;
     private StatusAgendamento status;
+
+    public static AgendamentDTO fromEntity( Agendamento agendamento ) {
+    
+        AgendamentDTO dto = new AgendamentDTO();
+
+        dto.setAgendamentoId( agendamento.getId() );
+        dto.setBarbeariaId( agendamento.getBarbearia().getId() );
+        dto.setBarbeiroId( agendamento.getBarbeiro().getId() );
+        dto.setClienteId( agendamento.getCliente().getId() );
+        dto.setData( agendamento.getData() );
+        dto.setHora( agendamento.getHora() );
+        dto.setServicoId( agendamento.getServico().getId() );
+        dto.setStatus( agendamento.getStatus() );
+
+        return dto;
+    }
 }
